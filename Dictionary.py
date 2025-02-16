98a9cff8e7b29eb245744de8238fd5707bf0f25d
AmbatLaurenceJimuel <laurencejimuelambat14@gmail.com>
2025-02-16 21:07:51 +0800

    Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,5 +1,36 @@
 # a list manipulation - dictionary
-DATA = { }
+DATA = { 
+  "Accentuate": "To make somthing more noticeable",
+ "Acquisce": "To accept something without protest, but reluctantly",
+"Amicable": "Characterized by good will and friendship",
+"Antedecent": "Preceding in time or order",
+"Benediction": "The act of bestowing or uttering a blessing, specially at the end of a religious service",
+"Berate": "To scold or criticize someone angrily",
+ "Beseech": "To ask someone urgently to do something",
+"Defer": "To postpone",
+"Discrete": "Individually distinct and seperate",
+"Eliducate": "To explain or make something clear",
+"Enchant": "To fill someone with great delight", "Ephemeral": "Lasting for a very short time",
+"Serendipity": "The development and occurence of events by chance in a happy or beneficial way",
+"Ubiquitious": "Present, appearing, or found somewhere",
+"Dogmatic": "Trusting one's opinion or beliefs while reluctant to accept those of others",
+"Abhor": "To hare or detest",
+"Appeace": "to calm or satisfy",
+"Avarice": "Excessive greed",
+"Book": "A collection of written pages bound together",
+"Betrayal": "The act of violating trust or confidence, or breaking a contract",
+"Bully": "To treat someone in an aggressive, cruel, or threatening way",
+"Celibate": "To abstain from sexual intercourse",
+"Cliche": "A trite or obvious remark",
+ "Coalation": "The union of diverse things into one group or form",
+  "Doppleganger": "A stranger who looks so much like another person that they could be twins",
+"Nefarious" : "Wicked, villainous, or extremely evil",
+"Halcyon": "Calm, peaceful, and carefree",
+"Sonorous": "Having a rich, full, and imposing sound",
+"Adjure": "To command someone as if under oath or the penalty of a curse",
+"Xenophobia": "The fear or hatred of people who are perceived as being different from oneself"
+"Electricity": "Used for appliances"
+}
 a=input(" SEARCH ")
 print(DATA[a])
 
